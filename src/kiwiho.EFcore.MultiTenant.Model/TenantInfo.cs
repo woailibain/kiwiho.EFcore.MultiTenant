09ed63eaf075e708909fb4f998675b1f7a2bb7d9
jun ho <jun@eyeconsys.com>
2020-03-05 21:03:05 +0800

    enhance  tenantInfo  to expandable model

--- src/kiwiho.EFcore.MultiTenant.Model/TenantInfo.cs
+++ src/kiwiho.EFcore.MultiTenant.Model/TenantInfo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Dynamic;
 
 namespace kiwiho.EFcore.MultiTenant.Model
@@ -9,6 +10,38 @@ namespace kiwiho.EFcore.MultiTenant.Model
 
         public bool IsPresent { get; set; }
 
-        public Type GeneratorType { get; set; }
+        public object Generator { get; set; }
+
+        public dynamic Container { get; set; } = new ExpandoObject();
+
+        Dictionary<string, object> dictionary = new Dictionary<string, object>();
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return dictionary.TryGetValue(binder.Name, out result);
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            dictionary[binder.Name] = value;
+            return true;
+        }
+
+        public override bool TrySetIndex(SetIndexBinder binder, object[] indexes, object value)
+        {
+            if (!(indexes?.Length > 0))
+                return false;
+            dictionary[indexes[0].ToString()] = value;
+            return true;
+        }
+
+        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
+        {
+            result = null;
+            if (!(indexes?.Length > 0))
+                return false;
+            result = dictionary[indexes[0].ToString()];
+            return true;
+        }
     }
 }
